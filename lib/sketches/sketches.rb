3577f656cd5434d67e7bd8a5db8f07943c9b1a98
postmodern <postmodern.mod3@gmail.com>
2009-05-25 18:35:35 -0700

    Added an example for Sketches.save.

--- lib/sketches/sketches.rb
+++ lib/sketches/sketches.rb
@@ -108,6 +108,8 @@ module Sketches
   # Saves the sketch with the specified _id_or_name_ to the specified
   # _path_.
   #
+  #   Sketches.save :foo, 'path/to/foo.rb'
+  #
   def Sketches.save(id_or_name,path)
     Sketches.cache.synchronize do
       if (sketch = Sketches.cache[id_or_name])
