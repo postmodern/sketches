00abbdfa6f016bb742b01cd0cc2261fa65691eaa
postmodern <postmodern.mod3@gmail.com>
2009-05-25 18:34:52 -0700

    Added documentation to the Kernel methods.

--- lib/sketches/extensions/kernel.rb
+++ lib/sketches/extensions/kernel.rb
@@ -23,22 +23,49 @@
 require 'sketches/sketches'
 
 module Kernel
+  #
+  # Edits the sketch with the specified _id_or_name_. If no sketch exists
+  # with the specified _id_or_name_, one will be created.
+  #
+  #   sketch 2
+  #
+  #   sketch :foo
+  #
   def sketch(id_or_name=nil)
     Sketches.sketch(id_or_name)
   end
 
+  #
+  # Creates a new sketch using the specified _path_.
+  #
+  #   sketch_from 'path/to/foo.rb'
+  #
   def sketch_from(path)
     Sketches.from(path)
   end
 
+  #
+  # Names the sketch with the specified _id_ with the specified _name_.
+  #
+  #   name_sketch 2, :foo
+  #
   def name_sketch(id,name)
     Sketches.name(id,name)
   end
 
+  #
+  # Saves the sketch with the specified _id_or_name_ to the specified
+  # _path_.
+  #
+  #   save_sketch :foo, 'path/to/foo.rb'
+  #
   def save_sketch(id_or_name,path)
     Sketches.save(id_or_name,path)
   end
 
+  #
+  # Print out all of the sketches.
+  #
   def sketches
     Sketches.print
   end
